7606c1c657beaf0c0d9a2cebbbe654f7191e9b61
Taylor Smith <tcs140230@utdallas.edu>
2018-04-21 21:37:48 -0400

    wired up added controls from UI to MainActivity and created number translator method

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -7,12 +7,29 @@ namespace Phoneword
     [Activity(Label = "Phoneword", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButon);
+
+            translateButton.Click += (sender, e) =>
+            {
+                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
         }
     }
 }
